a8b787940bfa73fad104d0cc33db2a00de9a2b06
Tommaso Turchi <github@tommasoturchi.com>
2025-05-11 09:48:54 +0200

    refactor: update face and image tracking examples to use Vite with improved structure and dependencies

--- examples/face-tracking/vite.config.js
+++ examples/face-tracking/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react(), mkcert()],
   server: {
     https: true
   }
